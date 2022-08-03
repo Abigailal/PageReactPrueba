e65f3f35073a0b95bb29b36720ef55c2b6ce08c1
Abigailal <abigail_al@outlook.com>
2022-08-03 12:29:13 -0500

    [ADD] Hook state, effect

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Caracters } from './components/Caracters';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HOLA MUNDO</h1>
+      <Header/>
+      <Caracters/>
+    </div>
+  );
+}
+
+export default App;
